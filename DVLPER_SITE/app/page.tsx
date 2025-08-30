70cd1981a4b2607e0cb42ba710898ab049cce045
narvindev <narvincg@gmail.com>
2025-08-30 18:32:00 +0530

    Add page.tsx file

---
+++ DVLPER_SITE/app/page.tsx
@@ -0,0 +1,19 @@
+import AboutMe from './_components/AboutMe';
+import Banner from './_components/Banner';
+import Experiences from './_components/Experiences';
+import Skills from './_components/Skills';
+import ProjectList from './_components/ProjectList';
+import Certificates from './_components/Certificates';
+
+export default function Home() {
+    return (
+        <div className="page-">
+            <Banner />
+            <AboutMe />
+            <Skills />
+            <Experiences />
+            <ProjectList />
+            <Certificates />
+        </div>
+    );
+}
